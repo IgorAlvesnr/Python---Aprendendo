252eafdc3691067e5cd49ce936b91c3e350fa9e3
Igor Alves <igorianrgit@gmail.com>
2025-04-23 19:02:27 -0300

    Aprendendo Variáveis

---
+++ Variaveis/variaveis.py
@@ -0,0 +1,13 @@
+media = 0 
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Igor', 33
+estado = True
+
+# Função type()
+# mostra qual o tipo da variavel
+
+print (type(media))  #int - inteiro
+print (type(n2))     #float
+print (type(nome))   #Str - string
+print (type(estado)) # bool - boleano
+print (type(1+2j))   # complex
