cc04e28e0317599de48d57d83f0dbbe87077f902
Igor Alves <igorianrgit@gmail.com>
2025-04-24 01:53:08 -0300

    Exercício de operadores aritméticos

---
+++ Operadores-Aritméticos/Exer01.py
@@ -0,0 +1,11 @@
+# Exercício 1: Calculadora Simples
+
+# Objetivo: Pedir dois números para o usuário e mostrar o resultado das 4 operações básicas entre eles.
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+print(f'A soma é: {float(num1 + num2)}')
+print(f'A subtração é: {float(num1 - num2)}')
+print(f'A multiplicação é: {float(num1 * num2)}')
+print(f'A divisão é: {float(num1 / num2)}')
