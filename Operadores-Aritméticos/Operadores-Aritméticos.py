4562c3f9a98ae1d0032452d47b20cd175f840e38
Igor Alves <igorianrgit@gmail.com>
2025-04-23 21:04:04 -0300

    Aula de Operadores Aritméticos

---
+++ Operadores-Aritméticos/Operadores-Aritméticos.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+print("Adição:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("Divisão inteira:", a // b)
+print("Resto da divisão:", a % b)
+print("Potência:", a ** b)
