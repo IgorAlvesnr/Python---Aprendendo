e43bd132fba2d18ffeddca938fe4ddbc598146a0
Igor Alves <igorianrgit@gmail.com>
2025-04-24 23:32:12 -0300

    Exemplos de Laços de repetição encadeado

---
+++ Laços-de-Repeticao-WHILE-FOR/Laços_encadeados.py
@@ -0,0 +1,15 @@
+for contEx in range(1,6):
+    print(f'\nRodada: {contEx}')
+    for contIn in range(5,0,-1):
+        print(f'Valor: {contIn}')
+
+print('Fim dos laços')
+
+import random
+
+for A in range(1,6):
+    print(f'\Conjunto {A}')
+    for B in range(5):
+        num = random.randint(1,100)
+        print(f'Valor: {num}')
+
